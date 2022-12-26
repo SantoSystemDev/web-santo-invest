f88e18c419737e569df3178b5e304df0522c3652
Santo System <kalangoti@gmail.com~>
2022-12-25 22:30:51 -0300

    feat: Add bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import "./assets/css/styles.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
