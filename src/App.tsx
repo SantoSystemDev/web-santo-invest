010f337048d363decdd4400fc254d3d0eff84d27
Santo System <kalangoti@gmail.com~>
2022-12-26 17:22:05 -0300

    feat: Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="container">
         <h1>Hello world</h1>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light mt-auto py-2">
+          <strong>
+            App desenvolvido por{" "}
+            <a href="https://github.com/santo-system" target="_blank" rel="noreferrer">
+              Santo System
+            </a>
+          </strong>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
